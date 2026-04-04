302fd5685a7221b12719b7ce21fac6f2866e9499
Maysara <Maysara.Elshewehy@gmail.com>
2026-04-04 10:08:50 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// src/index.ts
+//
+// Made with ❤️ by Maysara.
+
+
+
+// ╔════════════════════════════════════════ PACK ════════════════════════════════════════╗
+
+    import { cli }  from './core/builder';
+
+    export * from './core';
+    export * from './core/builder';
+    export * from './types';
+
+    export default cli;
+
+// ╚══════════════════════════════════════════════════════════════════════════════════════╝
